410090d2cb45c8d7c666c0807778cda7a57c0e13
ammon134 <ammon134@gmail.com>
2023-04-10 13:20:48 +0700

    move layer switching buttons further from resting position

--- keyboards/sofle/keymaps/quang-sofle/keymap.c
+++ keyboards/sofle/keymaps/quang-sofle/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-        KC_LGUI,KC_LALT,KC_LCTL, LT(_LOWER,KC_SPC), KC_SFTENT,     MT(MOD_LSFT, KC_BSPC), MO(_RAISE), KC_RCTL, TO(_LOWER), TO(_RAISE)
+KC_LGUI,KC_LALT,KC_LCTL, MT(MOD_LSFT, KC_SPC), LT(_LOWER,KC_ENT),     MO(_RAISE), MT(MOD_LSFT, KC_BSPC), KC_RCTL, TO(_LOWER), TO(_RAISE)
 ),
 /*
  * COLEMAK
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
   KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,      XXXXXXX,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-        KC_LGUI,KC_LALT,KC_LCTL, LT(_LOWER,KC_SPC), KC_SFTENT,    MT(MOD_LSFT, KC_BSPC), MO(_RAISE), KC_RCTL, TO(_LOWER), TO(_RAISE)
+KC_LGUI,KC_LALT,KC_LCTL, MT(MOD_LSFT, KC_SPC), LT(_LOWER,KC_ENT),     MO(_RAISE), MT(MOD_LSFT, KC_BSPC), KC_RCTL, TO(_LOWER), TO(_RAISE)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
