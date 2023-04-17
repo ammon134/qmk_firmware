475a4bf095ad0cf47671a6eccf3ad9982f22402c
ammon134 <ammon134@gmail.com>
2023-04-17 15:10:05 +0700

    add mod tap to esc key for screenshot hotkeys

--- keyboards/sofle/keymaps/quang-sofle/keymap.c
+++ keyboards/sofle/keymaps/quang-sofle/keymap.c
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINUS,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  MT(MOD_LCTL | MOD_LSFT, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-KC_LGUI,KC_LALT,KC_LCTL, LT(_LOWER, KC_SPC), KC_ENT,     MO(_RAISE), MT(MOD_LSFT, KC_BSPC), KC_RCTL, TO(_LOWER), TO(_RAISE)
+KC_LGUI,KC_LALT,CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), KC_ENT,     MO(_RAISE), MT(MOD_LSFT, KC_BSPC), KC_RCTL, TO(_LOWER), TO(_RAISE)
 ),
 /*
  * COLEMAK
@@ -62,11 +62,11 @@ KC_LGUI,KC_LALT,KC_LCTL, LT(_LOWER, KC_SPC), KC_ENT,     MO(_RAISE), MT(MOD_LSFT
  */
 
 [_COLEMAK] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINUS,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
-  KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
+  MT(MOD_LCTL | MOD_LSFT, KC_ESC), KC_A, KC_R, KC_S, KC_T, KC_G,          KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,      XXXXXXX,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-KC_LGUI,KC_LALT,KC_LCTL, LT(_LOWER, KC_SPC), KC_ENT,     MO(_RAISE), MT(MOD_LSFT, KC_BSPC), KC_RCTL, TO(_LOWER), TO(_RAISE)
+KC_LGUI,KC_LALT,CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), KC_ENT,     MO(_RAISE), MT(MOD_LSFT, KC_BSPC), KC_RCTL, TO(_LOWER), TO(_RAISE)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -86,7 +86,7 @@ KC_LGUI,KC_LALT,KC_LCTL, LT(_LOWER, KC_SPC), KC_ENT,     MO(_RAISE), MT(MOD_LSFT
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   _______,  KC_EXLM,  KC_AT, KC_UNDS,  KC_DLR, KC_PERC,                        KC_PLUS,  KC_7,   KC_8,    KC_9,  KC_ASTR, KC_BSPC,
   _______, KC_EQL, KC_COLN , KC_LPRN, KC_LCBR, KC_LBRC,                       KC_MINS,  KC_4,   KC_5,    KC_6,  KC_SLSH, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_RPRN, KC_RCBR, KC_RBRC, _______,       _______, KC_0,   KC_1,   KC_2,    KC_3,  KC_BSLS,  KC_F12,
+  _______,  KC_UNDS, KC_MINS, KC_RPRN, KC_RCBR, KC_RBRC, _______,       _______, KC_0,   KC_1,   KC_2,    KC_3,  KC_BSLS,  KC_F12,
                        _______, _______, _______, _______, _______,       _______, _______, _______, KC_PDOT, TO(0)
 ),
 /* RAISE
