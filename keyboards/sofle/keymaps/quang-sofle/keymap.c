b0fc634e7229cefb33b5107cabc86ea26be7788f
ammon134 <ammon134@gmail.com>
2023-05-17 20:13:35 +0700

    remove raycast from raise layer, add space to mo3

--- keyboards/sofle/keymaps/quang-sofle/keymap.c
+++ keyboards/sofle/keymaps/quang-sofle/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_GRV,   KC_1,   KC_2,     KC_3,   KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
 MT(MOD_LCTL|MOD_LSFT,KC_TAB), KC_Q,   KC_W,     KC_E,   KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     TO(_LOWER),
         MT(MOD_LGUI, KC_ESC), KC_A,   KC_S,     KC_D,   KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-                    KC_LSFT,  KC_Z,   KC_X,     KC_C,   KC_V,    KC_B, KC_MUTE,   XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-GUI_T(KC_LEFT),ALT_T(KC_RGHT),CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT),     MO(_RAISE), MT(MOD_RSFT, KC_BSPC), RCTL_T(KC_EQUAL), ALT_T(KC_UP), GUI_T(KC_DOWN)
+                    KC_LSFT,  KC_Z,   KC_X,     KC_C,   KC_V,    KC_B, KC_MUTE,   KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+GUI_T(KC_LEFT),ALT_T(KC_RGHT),CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT),     LT(_RAISE, KC_SPC), MT(MOD_RSFT, KC_BSPC), RCTL_T(KC_EQUAL), ALT_T(KC_UP), GUI_T(KC_DOWN)
 ),
 /*
  * COLEMAK
@@ -65,8 +65,8 @@ GUI_T(KC_LEFT),ALT_T(KC_RGHT),CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), LT(_RAISE, KC
                     KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
 MT(MOD_LCTL|MOD_LSFT,KC_TAB), KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  TO(_LOWER),
         MT(MOD_LGUI, KC_ESC), KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT,
-                    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,    XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-GUI_T(KC_LEFT),ALT_T(KC_RGHT),CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT),     MO(_RAISE), MT(MOD_RSFT, KC_BSPC), RCTL_T(KC_EQUAL), ALT_T(KC_UP), GUI_T(KC_DOWN)
+                    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,    KC_MPLY, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+GUI_T(KC_LEFT),ALT_T(KC_RGHT),CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT),     LT(_RAISE, KC_SPC), MT(MOD_RSFT, KC_BSPC), RCTL_T(KC_EQUAL), ALT_T(KC_UP), GUI_T(KC_DOWN)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -87,7 +87,7 @@ GUI_T(KC_LEFT),ALT_T(KC_RGHT),CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), LT(_RAISE, KC
     _______,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_PLUS,  KC_7,   KC_8,    KC_9,   KC_ASTR, TO(_RAISE),
     TO(0)  ,   KC_AMPR, KC_COLN, KC_LPRN, KC_LCBR, KC_LBRC,                         KC_MINS,  KC_4,   KC_5,    KC_6,   KC_SLSH, KC_PIPE,
     _______,   KC_CIRC, KC_UNDS, KC_RPRN, KC_RCBR, KC_RBRC, KC_MPLY,       _______, KC_0,     KC_1,   KC_2,    KC_3,   KC_BSLS, KC_F12,
-                        _______, _______, _______, _______, _______,       _______, _______, KC_PDOT, _______, _______
+                        _______, _______, _______, _______, _______,      _______, _______, KC_PDOT, _______, _______
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
@@ -105,8 +105,8 @@ GUI_T(KC_LEFT),ALT_T(KC_RGHT),CTL_T(KC_MINUS), LT(_LOWER, KC_SPC), LT(_RAISE, KC
  */
 [_RAISE] = LAYOUT(
     _______, _______ , _______ , _______ ,   _______ , _______,                          _______,     _______,    _______, _______,  _______ ,    _______,
-    _______, XXXXXXX, A(KC_SPC), C(KC_SPC),  XXXXXXX,  XXXXXXX,                          A(KC_UP),    KC_PRVWD,   KC_UP,   KC_NXTWD, A(KC_BSPC),  TO(_ADJUST),
-    TO(0)  , C(KC_A), KC_LCTL,   KC_LSFT,    KC_LALT,  KC_CAPS,                          A(KC_DOWN),  KC_LEFT,    KC_DOWN, KC_RGHT,  XXXXXXX,     KC_BSPC,
+    _______, XXXXXXX, C(KC_W),   XXXXXXX,    XXXXXXX,  XXXXXXX,                          A(KC_UP),    KC_PRVWD,   KC_UP,   KC_NXTWD, A(KC_BSPC),  TO(_ADJUST),
+    TO(0)  , C(KC_A), KC_LSFT,   KC_LGUI,    KC_LALT,  KC_CAPS,                          A(KC_DOWN),  KC_LEFT,    KC_DOWN, KC_RGHT,  XXXXXXX,     KC_BSPC,
     _______, KC_UNDO, KC_CUT,    KC_COPY,    KC_PASTE, KC_REDO,  KC_MPLY,       _______, KC_DEL,      KC_LSTRT,   XXXXXXX, KC_LEND,  KC_DLINE,    _______,
                             _______, _______, _______, _______, _______,       _______, _______, _______, _______, XXXXXXX
 ),
