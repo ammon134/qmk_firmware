b32c2e2d7b9195624f34b730a35714732ca77f84
ammon134 <ammon134@gmail.com>
2023-08-10 19:49:29 +0700

    create main branch fresh

---
+++ keyboards/sofle/keymaps/quang-sofle/config.h
@@ -0,0 +1,46 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+
+	//#define RGBLIGHT_EFFECT_BREATHING
+	#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+	//#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+	//#define RGBLIGHT_EFFECT_SNAKE
+	//#define RGBLIGHT_EFFECT_KNIGHT
+	//#define RGBLIGHT_EFFECT_CHRISTMAS
+	//#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+	//#define RGBLIGHT_EFFECT_RGB_TEST
+	//#define RGBLIGHT_EFFECT_ALTERNATING
+	//#define RGBLIGHT_EFFECT_TWINKLE
+
+    #define RGBLED_NUM 70
+	//#define RGBLED_SPLIT
+	#define RGBLED_SPLIT { 35, 35 } // haven't figured out how to use this yet
+
+	//#define RGBLED_NUM 30
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
+
+/* ws2812 RGB LED */
+#define RGB_DI_PIN D3
+
+// Fine tuning space fn tap hold timing
+#define QUICK_TAP_TERM_PER_KEY
+
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+    #define TAPPING_TERM 175
+#endif
+// #define PERMISSIVE_HOLD
+
+// // Turn on CAPS WORD
+// #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
